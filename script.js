58e0d8526e84368588beb948d953552b68a92fd2
rafaseoanes <rafaseoanes9@gmail.com>
2024-04-05 16:37:49 -0500

    Basic funtions created for each of the math operators

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log("hey")
+
+
+// add
+// subtract
+// multiply
+// divide
+
+//.toFixed(5)
+let firtsNum = 0
+let sencondNum = 0
+let operator = ""
+
+const add = function (firtsUserInput, secondUserInput) {
+    return parseInt(firtsUserInput) + parseInt(secondUserInput)
+}
+
+const subtract = function (firtsUserInput, secondUserInput) {
+    return parseInt(firtsUserInput) - parseInt(secondUserInput)
+}
+
+const multiply = function (firtsUserInput, secondUserInput) {
+    return parseInt(firtsUserInput) * parseInt(secondUserInput)
+}
+
+const divide = function (firtsUserInput, secondUserInput) {
+    let results = parseInt(firtsUserInput) / parseInt(secondUserInput)
+    return results.toFixed(5)
+}
+
+
+
+const operate = (firtsNum, sencondNum, operator){
+    
+}
+
+console.log(divide("53", 6))
