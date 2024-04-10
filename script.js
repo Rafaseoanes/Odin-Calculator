f312cbeea67b6fabecca6b80abfc3acdc5af558c
rafaseoanes <rafaseoanes9@gmail.com>
2024-04-09 21:42:54 -0500

    Comments added

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
+//Main variables for the calculator:
 let firtsNum = ""
 let secondNum = ""
 let operator = ""
 
+//Main functions for each operation:
 const add = function (firtsUserInput, secondUserInput) {
     return parseInt(firtsUserInput) + parseInt(secondUserInput)
 }
@@ -21,8 +23,9 @@ const divide = function (firtsUserInput, secondUserInput) {
 }
 
 
-
+//This is the principal funtion wich takes each value and operate based on the operator variable:
 const operate = (firtsNum, operator, secondNum) => {
+
     switch (operator) {
         case "+":
             return add(firtsNum, secondNum)
@@ -34,12 +37,17 @@ const operate = (firtsNum, operator, secondNum) => {
             return multiply(firtsNum, secondNum)
 
         case "/":
+            if (secondNum == "0") {
+
+                return "Oooops!!"
+            }
             return divide(firtsNum, secondNum)
 
 
     }
 }
 
+//wiring of the buttons in the front end with the back end:
 const buttons = document.getElementsByClassName("calButton")
 const displayText = document.getElementById("displayText")
 
@@ -52,9 +60,15 @@ for (let button of buttons) {
     })
 }
 
+
+//The updateDisplay function handles the display of the calculator when a button is pressed
 function updateDisplay(value) {
 
     if (value == "+" || value == "-" || value == "*" || value == "/") {
+        if (firtsNum && secondNum && operator) {
+            // console.log(firtsNum, secondNum, operator)
+            getResults()
+        }
         operator = value
         // console.log(operator)
 
@@ -71,6 +85,7 @@ function updateDisplay(value) {
 
 }
 
+//Funtionality for the clearKey Button:
 const clearKey = document.getElementById("clearKey")
 
 clearKey.addEventListener("click", () => {
@@ -82,13 +97,22 @@ clearKey.addEventListener("click", () => {
 
 const equalsKey = document.getElementById("equalsKey")
 
-equalsKey.addEventListener("click", () => {
+equalsKey.addEventListener("click", getResults)
+
+
+
+function getResults() {
     let result = operate(firtsNum, operator, secondNum)
     // console.log(result)
     displayText.innerText = result
-    firtsNum = result
-    secondNum = ""
-    operator = ""
-    // updateDisplay(result)
+    if (result == "Oooops!!") {
+        firtsNum = "0"
+        secondNum = ""
+        operator = ""
+    } else {
 
-})
+        firtsNum = result
+        secondNum = ""
+        operator = ""
+    }
+}
